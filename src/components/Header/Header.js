40ff30af75941131085a0af4d0d65735596675db
wavinginspace <paulbaisley@gmail.com>
2020-03-16 19:20:01 -0400

    started styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,26 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.scss';
 
+
 export class Header extends Component {
+
+  state = {
+    loading: true
+  }
+
+  componentDidMount() {
+    
+    this.setState({
+      loading: false
+    })
+  }
+
   render() {
+    if (this.state.loading) {
+      return <></>
+    }
+
+
     return (
       <div className="Header">
         <header>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
 import './index.scss';
+import WebfontLoader from '@dr-kobros/react-webfont-loader';
+
+
+const config = {
+  google: {
+    families: ['Fredoka One', 'Roboto Mono'],
+  }
+};
 
 ReactDOM.render(
   <BrowserRouter>
+  <WebfontLoader config={config}>
     <App />
+    </WebfontLoader>
   </BrowserRouter>,
   document.getElementById('root')
 );
